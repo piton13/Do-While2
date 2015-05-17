b6c345661f3dfff094b65633ec024f07f9c2880f
piton13 <igor13luk@gmail.com>
2015-05-17 21:44:35 +0300

    [Taks list] Fixed some Denys's and Borys's remarks

--- client/js/app/main/modules/today/today.controller.js
+++ client/js/app/main/modules/today/today.controller.js
@@ -1,12 +1,18 @@
 /**
  * @ngInject
  */
-module.exports = function (todayService, $filter, todayDate) {
-    var vm = this;
+module.exports = function (todayService) {
+    var vm = this,
+        todayDate = new Date(),
+        todayJSON = todayDate.toJSON().substr(0,10);
+    this.todayDate = todayDate;
     this.tasks = [];
-    this.todayDateLong = $filter('date')(todayDate, "d MMM yyyy").toLowerCase();
 
     todayService.getTasks(function (res) {
-        vm.tasks = res.data;
+        for (var i=0; i<res.data.length; i++){
+          if (res.data[i].date.substr(0,10) === todayJSON) {
+            vm.tasks.push(res.data[i]);
+          }
+        }
     });
 };
