2159b164b7f167acba5ff9f0d79263da84eae9fa
piton13 <igor13luk@gmail.com>
2015-05-20 00:23:19 +0300

    [Week list] Fixed Borys's remarks: Divide controller job to service

--- client/js/app/main/modules/week/index.js
+++ client/js/app/main/modules/week/index.js
@@ -3,7 +3,6 @@ var angular = require('angular');
 var weekModule = angular.module('weekModule', [
     require('./weekNav')
 ])
-    .service('weekService', require('./week.service'))
     .config(require('./week.config'));
 
 module.exports = weekModule.name;
--- client/js/app/main/modules/week/week.controller.js
+++ client/js/app/main/modules/week/week.controller.js
@@ -2,29 +2,11 @@
  * @ngInject
  */
 module.exports = function (todayService) {
-    var vm = this,
-        oneDay = 86400000,
-        timeZone = 1000*60*60*3,
-        todayDate = new Date(),
-        parsedDate = Date.parse(todayDate);
-    this.todayDate = todayDate;
+    var vm = this;
+    this.todayDate = new Date();
     this.weekDays = [];
 
-    for (var i=0; i<7; i++) {
-      vm.weekDays.push({
-        date: new Date(parsedDate+i*oneDay+timeZone).toJSON().substr(0,10),
-        tasks: []
-      });
-    }
-
-    todayService.getTasks(function (res) {
-
-        for (var i=0; i<7; i++) {
-          for (var j=0; j<res.data.length; j++){
-            if (res.data[j].date.substr(0,10) === vm.weekDays[i].date) {
-              vm.weekDays[i].tasks.push(res.data[j]);
-            }
-          }
-        }
+    todayService.getWeekTasks().success(function (tasks) {
+        vm.weekDays = tasks;
     });
 };
