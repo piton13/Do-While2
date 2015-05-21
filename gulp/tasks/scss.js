6db380c03c47c3732069a5997ee9e0dd0e322620
Borys Ihnatyev <paur.link@gmail.com>
2015-05-22 00:23:33 +0300

    [Automation][Post css] - Add autoprefixer in build:scss task.

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,16 +1,19 @@
 var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
     del = require('del'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync'),
-    path = require('path'),
     config = require('../config'),
     errorDebug = require('../lib/error-debug');
 
 function buildScss() {
     return gulp.src(config.paths.src.scss.main)
         .pipe(sass({errLogToConsole: true}))
+        .pipe(autoprefixer({
+            browsers: ['> 5%', 'last 2 version', 'IE 9']
+        }))
         .pipe(gulp.dest(config.paths.dist.css))
-        .pipe(browserSync.reload({stream:true}));
+        .pipe(browserSync.reload({stream: true}));
 }
 
 gulp.task('build:scss', ['build:img:icons', 'clean:css'], function () {
