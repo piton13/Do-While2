48d114314b875baa099d6abf8184c3e27f189ef7
Borys Ihnatyev <paur.link@gmail.com>
2015-05-28 16:33:59 +0300

    [SignIn][Server] added method override dependency.

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,14 +1,15 @@
 var session = require('express-session'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 module.exports = function (app) {
     app.use(logger('dev'));
+    app.use(cookieParser());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(session({secret: 'keyboard cat'}));
-    app.use(cookieParser());
-
     require('./passport')(app);
+    app.use(methodOverride());
 };
