f4169073084c4767ad7d2ff89026c6976dab1d19
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2015-05-31 01:12:32 +0300

    Update project list

--- client/js/app/main/modules/menu/projectList/projectList.directive.js
+++ client/js/app/main/modules/menu/projectList/projectList.directive.js
@@ -6,6 +6,8 @@ module.exports = function () {
         restrict: 'E',
         replace: true,
         transclude: true,
+        controller: require('./projectList.directive.controller'),
+        controllerAs: 'projectList',
         templateUrl: 'projectList.template.html'
     };
 };
