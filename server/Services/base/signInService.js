de22b1750e83d442808cc870d6f7e5cddd6c8cd0
Borys Ihnatyev <paur.link@gmail.com>
2015-06-01 21:38:02 +0300

    [Server][SignIn] - Refresh access_token on re sign in.

--- server/Services/base/signInService.js
+++ server/Services/base/signInService.js
@@ -1,4 +1,5 @@
-var userAccountService = require('../userAccountService');
+var userAccountService = require('../userAccountService'),
+    Q = require('q');
 
 function SignInService() {
 }
@@ -7,6 +8,10 @@ SignInService.prototype._onAccountRetrieved = function (token, account) {
     var accessToken = token.access_token;
     account = this._normalizeAccount(account);
     return userAccountService.getUserByAccount(account)
+        .then(function (user) {
+            user.access_token = accessToken;
+            return Q(user.save());
+        })
         .fail(function (err) {
             if (err.message === 'no account')
                 return userAccountService.createUserByAccount(account, accessToken);
